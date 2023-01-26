56fe5b8ddb1aaf914980e4ce83b40abce4312474
Andrey Zhukov <ch.darv@icloud.com>
2023-01-26 13:55:14 +1200

    update

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,32 +1,52 @@
 const sliderArrowPrev = document.querySelector(".slider__arrow_prev");
 const sliderArrowNext = document.querySelector(".slider__arrow_next");
-const sliderItems = document.querySelectorAll(".slider__item");
-let current = 0;
+const sliderDots = document.querySelector('.slider__dots')
+const sliderItems = Array.from(document.querySelectorAll(".slider__item"));
+const dotItems = Array.from(sliderDots.children)
 
-sliderArrowPrev.addEventListener("click", function () {
-    if (current - 1 === -1) {
-        current = sliderItems.length - 1;
-    } else {
-        current -= 1;
-    }
-    slider();
-});
+sliderDots.firstElementChild.classList.add('slider__dot_active')
 
-sliderArrowNext.addEventListener("click", function () {
-    if (current + 1 === sliderItems.length) {
-        current = 0;
-    } else {
-        current += 1;
-    }
-    slider();
-});
+const switchToRight = () => {
+  let currentSliderIndex = sliderItems.findIndex(slide => slide.classList.contains('slider__item_active'));
+  sliderItems.forEach(slider => slider.classList.remove('slider__item_active'));
+  dotItems.forEach(dot => dot.classList.remove('slider__dot_active'));
+  currentSliderIndex++;
 
-const slider = () => {
-    const arrSliderItems = Array.from(sliderItems);
+  if (currentSliderIndex === sliderItems.length) {
+    sliderItems[0].classList.add("slider__item_active");
+    dotItems[0].classList.add('slider__dot_active');
+  } else {
+    sliderItems[currentSliderIndex].classList.add("slider__item_active");
+    dotItems[currentSliderIndex].classList.add('slider__dot_active');
+  }
+}
 
-    for (let i = 0; i < arrSliderItems.length; i++) {
-        arrSliderItems[i].classList.remove("slider__item_active");
-    }
+const switchToLeft = () => {
+  let currentSliderIndex = sliderItems.findIndex(slide => slide.classList.contains('slider__item_active'));
+  sliderItems.forEach(slider => slider.classList.remove('slider__item_active'));
+  dotItems.forEach(dot => dot.classList.remove('slider__dot_active'));
+  currentSliderIndex--;
 
-    arrSliderItems[current].classList.add("slider__item_active");
-};
+  if (currentSliderIndex < 0) {
+    sliderItems[sliderItems.length - 1].classList.add("slider__item_active")
+    dotItems[sliderItems.length - 1].classList.add('slider__dot_active');
+  } else {
+    sliderItems[currentSliderIndex].classList.add("slider__item_active");
+    dotItems[currentSliderIndex].classList.add('slider__dot_active');
+  }
+}
+
+const switchDots = (e) => {
+  if (!e.target.classList.contains('slider__dot')) return;
+
+  let currentDotIndex = dotItems.findIndex(dot => dot === e.target);
+
+  dotItems.forEach(dot => dot.classList.remove('slider__dot_active'));
+  sliderItems.forEach(slider => slider.classList.remove('slider__item_active'));
+  dotItems[currentDotIndex].classList.add('slider__dot_active');
+  sliderItems[currentDotIndex].classList.add('slider__item_active');
+}
+
+sliderArrowNext.addEventListener('click', switchToRight);
+sliderArrowPrev.addEventListener('click', switchToLeft);
+sliderDots.addEventListener('click', switchDots)
