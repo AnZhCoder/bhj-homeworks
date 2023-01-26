ec174106a804c7ede641e1d5300bed90a9bf154c
Andrey Zhukov <ch.darv@icloud.com>
2023-01-26 23:09:51 +1200

    Edited task.js

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -4,49 +4,43 @@ const sliderDots = document.querySelector('.slider__dots')
 const sliderItems = Array.from(document.querySelectorAll(".slider__item"));
 const dotItems = Array.from(sliderDots.children)
 
-sliderDots.firstElementChild.classList.add('slider__dot_active')
+sliderDots.firstElementChild.classList.add('slider__dot_active');
 
-const switchToRight = () => {
-  let currentSliderIndex = sliderItems.findIndex(slide => slide.classList.contains('slider__item_active'));
-  sliderItems.forEach(slider => slider.classList.remove('slider__item_active'));
-  dotItems.forEach(dot => dot.classList.remove('slider__dot_active'));
-  currentSliderIndex++;
+const switchSlider = (indexNextSlide) => {
+  let index = indexNextSlide;
+  const currentSliderIndex = sliderItems.findIndex(slide => slide.classList.contains('slider__item_active'));
+  sliderItems[currentSliderIndex].classList.remove('slider__item_active');
+  dotItems[currentSliderIndex].classList.remove('slider__dot_active');
 
-  if (currentSliderIndex === sliderItems.length) {
-    sliderItems[0].classList.add("slider__item_active");
-    dotItems[0].classList.add('slider__dot_active');
-  } else {
-    sliderItems[currentSliderIndex].classList.add("slider__item_active");
-    dotItems[currentSliderIndex].classList.add('slider__dot_active');
+  if (index >= sliderItems.length) {
+    index = 0;
   }
-}
 
-const switchToLeft = () => {
-  let currentSliderIndex = sliderItems.findIndex(slide => slide.classList.contains('slider__item_active'));
-  sliderItems.forEach(slider => slider.classList.remove('slider__item_active'));
-  dotItems.forEach(dot => dot.classList.remove('slider__dot_active'));
-  currentSliderIndex--;
-
-  if (currentSliderIndex < 0) {
-    sliderItems[sliderItems.length - 1].classList.add("slider__item_active")
-    dotItems[sliderItems.length - 1].classList.add('slider__dot_active');
-  } else {
-    sliderItems[currentSliderIndex].classList.add("slider__item_active");
-    dotItems[currentSliderIndex].classList.add('slider__dot_active');
+  if (index < 0) {
+    index = sliderItems.length - 1;
   }
+
+  sliderItems[index].classList.add('slider__item_active');
+  dotItems[index].classList.add('slider__dot_active');
 }
 
-const switchDots = (e) => {
-  if (!e.target.classList.contains('slider__dot')) return;
 
-  let currentDotIndex = dotItems.findIndex(dot => dot === e.target);
 
-  dotItems.forEach(dot => dot.classList.remove('slider__dot_active'));
-  sliderItems.forEach(slider => slider.classList.remove('slider__item_active'));
-  dotItems[currentDotIndex].classList.add('slider__dot_active');
-  sliderItems[currentDotIndex].classList.add('slider__item_active');
-}
+sliderDots.addEventListener('click', e => {
+  if (!e.target.classList.contains('slider__dot')) {
+    return
+  }
+  let indexNextSlide = dotItems.findIndex(dot => dot === e.target);
+  switchSlider(indexNextSlide);
+})
 
-sliderArrowNext.addEventListener('click', switchToRight);
-sliderArrowPrev.addEventListener('click', switchToLeft);
-sliderDots.addEventListener('click', switchDots)
+sliderArrowNext.addEventListener('click', () => {
+  let indexNextSlide = (sliderItems.findIndex(slide => slide.classList.contains('slider__item_active'))) + 1;
+  switchSlider(indexNextSlide);
+});
+
+
+sliderArrowPrev.addEventListener('click', () => {
+  let indexNextSlide = (sliderItems.findIndex(slide => slide.classList.contains('slider__item_active'))) - 1;
+  switchSlider(indexNextSlide)
+});
