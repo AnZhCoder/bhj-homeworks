ee762e28b11342e42e23dedcbea5ca80fcf4f79a
Andrey Zhukov <ch.darv@icloud.com>
2023-01-26 23:08:59 +1200

    Edited task.js

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,20 +2,23 @@ const menuSubs = document.querySelectorAll('.menu_sub');
 const menu = document.querySelector('.menu');
 
 const switchMenu = (e) => {
-  if (!e.target.classList.contains('menu__link')) return;
+  if (!e.target.classList.contains('menu__link')) {
+    return;
+  }
 
   const currentLink = e.target;
   let subMenu = currentLink.closest('.menu__item').querySelector('.menu_sub');
 
-  if (subMenu) e.preventDefault();
+  if (subMenu) {
+    e.preventDefault();
+    subMenu.classList.toggle('menu_active');
+  }
 
   menuSubs.forEach((item) => {
     if (item !== subMenu) {
       item.classList.remove('menu_active');
     }
   });
-
-  subMenu.classList.toggle('menu_active');
 }
 
 menu.addEventListener('click', switchMenu)
