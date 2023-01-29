d147293f69500345836144970685821dcbea63fd
Andrey Zhukov <ch.darv@icloud.com>
2023-01-29 12:22:50 +1200

    Edited task.js

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,21 +1,17 @@
 const dropDownValue = document.querySelector('.dropdown__value');
 const dropDownList = document.querySelectorAll('.dropdown__list');
 const dropDownItem = document.querySelectorAll('.dropdown__item');
-const dropDownLink = document.querySelectorAll('.dropdown__link');
 
-
-const switchMenu =() => {
+const switchMenu = () => {
     dropDownList.forEach(item => {
         item.classList.toggle('dropdown__list_active');
     })
 }
 
-dropDownValue.addEventListener('click', function(){
-        switchMenu();
-});
+dropDownValue.addEventListener('click', switchMenu);
 
 dropDownItem.forEach(el => {
-    el.addEventListener('click', function(event){
+    el.addEventListener('click', function (event) {
         event.preventDefault();
         dropDownValue.textContent = el.textContent;
     })
