d7679f10ce64f73e7e39eea522f368006fe73358
Andrey Zhukov <ch.darv@icloud.com>
2023-02-02 13:30:29 +1200

    fixed task.js

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,20 +1,22 @@
 const dropDownValue = document.querySelector('.dropdown__value');
-const dropDownList = document.querySelectorAll('.dropdown__list');
+const dropDownList = document.querySelector('.dropdown__list');
 const dropDownItem = document.querySelectorAll('.dropdown__item');
 
-const switchMenu = () => {
-    dropDownList.forEach(item => {
-        item.classList.toggle('dropdown__list_active');
-    })
+const switchMenu = (e) => {
+    if (e.target.classList.contains('dropdown__value')) {
+        e.target.nextElementSibling.classList.toggle('dropdown__list_active');
+    }
+}
+
+const switchValue = (e) => {
+    if (!e.target.classList.contains('dropdown__link')) {
+        return;
+    }
+
+    e.preventDefault();
+    e.target.closest('.dropdown').querySelector('.dropdown__value').textContent = e.target.textContent;
+    e.target.closest('.dropdown').querySelector('.dropdown__list').classList.remove('dropdown__list_active');
 }
 
 dropDownValue.addEventListener('click', switchMenu);
-
-dropDownItem.forEach(el => {
-    el.addEventListener('click', function (event) {
-        event.preventDefault();
-        dropDownValue.textContent = el.textContent;
-    })
-})
-
-
+dropDownList.addEventListener('click', switchValue);
